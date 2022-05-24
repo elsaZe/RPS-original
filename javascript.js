3e4aebefc00b905cc5de4883fea746ac96976edc
Elsa Zeigler <elsa.head@gmail.com>
2022-05-23 20:32:42 -0400

    Play a round between computer and user

--- javascript.js
+++ javascript.js
@@ -11,4 +11,43 @@ function computerPlay() {
     return computerChoice
 }
 
-console.log(computerPlay())
+
+
+let result;
+let playerSelection;
+
+function singleRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    console.log(playerSelection)
+    console.log(computerSelection)
+    if (playerSelection == 'rock') {
+        if (computerSelection == 'rock') {
+            result = "tie"
+        } else if (computerSelection == 'paper') {
+            result = 'loss'
+        } else if (computerSelection == 'scissors') {
+            result = 'win'
+        }
+    }
+    if (playerSelection == 'paper') {
+        if (computerSelection == 'rock') {
+            result = "win"
+        } else if (computerSelection == 'paper') {
+            result = 'tie'
+        } else if (computerSelection == 'scissors') {
+            result = 'loss'
+        }
+    }
+    if (playerSelection == 'scissors') {
+        if (computerSelection == 'rock') {
+            result = "loss"
+        } else if (computerSelection == 'paper') {
+            result = 'win'
+        } else if (computerSelection == 'scissors') {
+            result = 'tie'
+        }
+    }
+    return result;
+}
+
+console.log(singleRound('paper', computerPlay()))
